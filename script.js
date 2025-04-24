eac2572b1759a47f4bc392e1e8c850b21c37ef72
luiz-flamorim <luiz.flamorim@gmail.com>
2025-04-24 17:00:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let ws = new WebSocket(webSocketurl);
+
+let nightmareBox = document.querySelector("#nightmare");
+nightmareBox.addEventListener(
+	"input",
+	(event) => {
+		ws.send(JSON.stringify({ prompt: nightmareBox.value }));
+	},
+	false
+);
+
+let controlledByTD = document.querySelector(".controlledByTD");
+
+ws.addEventListener("open", (event) => {
+	console.log("Socket connection open");
+	// alert('Successfully connected to socket server 🎉');
+	ws.send("pong");
+});
+
+ws.addEventListener("error", (error) => {
+	console.error("Error in the connection", error);
+	alert("error connecting socket server", error);
+});
+
+ws.addEventListener("close", (event) => {
+	console.log("Socket connection closed");
+	alert("closing socket server");
+});
